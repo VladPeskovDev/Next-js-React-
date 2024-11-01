764448c9ac83c78b02b39f5804642f839e105baf
Владислав <vladislav@MacBook-Air-Vladislav.local>
2024-11-01 17:15:26 +0300

    Release0.6

--- lawyer-page/src/app/contact/page.tsx
+++ lawyer-page/src/app/contact/page.tsx
@@ -1,14 +1,37 @@
-export const metadata = {
-    title: "Адвокат по уголовным делам | Контакты",
-    description: "Свяжитесь с адвокатом по уголовным делам. Контактная информация.",
-  };
-  
-  export default function ContactPage(): JSX.Element {
-    return (
-      <main className="container mx-auto p-4">
-        <h1>Контакты</h1>
-        <p>Для связи с адвокатом воспользуйтесь контактной информацией, указанной здесь.</p>
-      </main>
-    );
-  }
-  
+'use client';
+
+import styles from './ContactPage.module.css';
+import { FaTelegramPlane } from 'react-icons/fa';
+
+
+
+export default function ContactPage(): JSX.Element {
+  return (
+    <main className={styles.container}>
+      <div className={styles.photoContainer}>
+        <div className={styles.photoOverlay}>
+          <p className={styles.overlayText}>Если Вам нужна помощь, контакты для консультации</p>
+          <div className={styles.buttons}>
+            <a href="tel:+79165780936" className={styles.button}>+7 916 578 09 36</a>
+            <a href="https://t.me/VladislavPeskov" target="_blank" rel="noopener noreferrer" className={styles.buttonTelegram}>
+            <FaTelegramPlane className={styles.icon}/>Telegram</a>
+          </div>
+        </div>
+      </div>
+      
+      <div className={styles.infoSection}>
+        <div className={styles.infoWrapper}>
+          <div className={styles.card}>
+            <p>Адрес: г. Москва, ул. Маршала Рыбалко 2, корп.1-9, оф.408</p>
+          </div>
+          <div className={styles.card}>
+            <p>Телефон: +7 916 578 09 36</p>
+          </div>
+          <div className={styles.card}>
+            <p>Email: peskov_lawyer@mail.ru</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
