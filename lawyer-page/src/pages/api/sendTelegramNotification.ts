b5e54e09ecbba1fdfd00484111eaefaec995510d
Владислав <vladislav@MacBook-Air-Vladislav.local>
2024-11-11 14:16:43 +0300

    Release2.3

--- lawyer-page/src/pages/api/sendTelegramNotification.ts
+++ lawyer-page/src/pages/api/sendTelegramNotification.ts
@@ -7,14 +7,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const botToken = process.env.TELEGRAM_BOT_TOKEN1;
     const chatId = process.env.TELEGRAM_CHAT_ID;
 
+    console.log("Получено сообщение:", message);
+    console.log("Токен и чат ID:", { botToken, chatId });
+
     if (!botToken || !chatId) {
+      console.error("Ошибка: TELEGRAM_BOT_TOKEN1 или TELEGRAM_CHAT_ID отсутствуют");
       return res.status(500).json({ error: 'Телеграм токен или ID чата отсутствуют' });
     }
 
     const telegramUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
     try {
-      await fetch(telegramUrl, {
+      const response = await fetch(telegramUrl, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -25,6 +29,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }),
       });
 
+      if (!response.ok) {
+        const errorText = await response.text();
+        console.error("Ошибка ответа от Telegram API:", errorText);
+        return res.status(500).json({ error: 'Ошибка отправки сообщения в Telegram' });
+      }
+
+      console.log("Сообщение успешно отправлено в Telegram");
       res.status(200).json({ success: 'Сообщение отправлено в Telegram' });
     } catch (error) {
       console.error("Ошибка при отправке сообщения в Telegram:", error);
