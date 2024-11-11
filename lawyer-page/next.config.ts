32baebcddc410b8155b47110fdf0576727f4dda9
VladPeskovDev <vladpeskovdev@gmail.com>
2024-11-11 13:41:15 +0300

    Release2.0

--- lawyer-page/next.config.ts
+++ lawyer-page/next.config.ts
@@ -1,8 +1,38 @@
+//import type { NextConfig } from "next";
+
+//const nextConfig: NextConfig = {
+  /* config options here */
+  //reactStrictMode: false,
+//};
+
+//export default nextConfig; 
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        // Добавляем настройки CORS для всех API-маршрутов
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // или укажите ваш домен для повышения безопасности, например, "https://advocatpeskov.com"
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
