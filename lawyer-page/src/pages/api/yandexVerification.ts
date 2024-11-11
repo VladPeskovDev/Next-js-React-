22ba6de2acfa52f8e7f6ef5a245d3d2bbfcac30d
VladPeskovDev <vladpeskovdev@gmail.com>
2024-11-11 17:50:51 +0300

    Release3.2

--- lawyer-page/src/pages/api/yandexVerification.ts
+++ lawyer-page/src/pages/api/yandexVerification.ts
@@ -1,6 +1,7 @@
+// pages/api/yandexVerification.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('Content-Type', 'text/html');
-  res.status(200).send('yandex_ca6674660fe1aaf4');
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(200).send('ca6674660fe1aaf4'); // строгое текстовое содержимое
 }
