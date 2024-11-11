1594c305da16c9a2c2f6ca79e76d23719bea0eb1
Владислав <vladislav@MacBook-Air-Vladislav.local>
2024-11-11 16:58:15 +0300

    Release2.9

--- lawyer-page/src/app/page.tsx
+++ lawyer-page/src/app/page.tsx
@@ -1,28 +1,35 @@
 import styles from "./HomePage.module.css";
 import { FaGavel, FaBook, FaUserShield, FaFileAlt } from "react-icons/fa";
 import Link from "next/link";
-import type { Metadata } from "next";
-
-
-export const metadata: Metadata = {
-  title: "Адвокат по уголовным делам | Главная",
-  description: "Адвокат по уголовным делам в Москве – помощь и консультации.",
-};
+import Head from "next/head";
 
 export default function HomePage(): JSX.Element {
- 
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Адвокат по уголовным делам | Главная</title>
+        <meta
+          name="description"
+          content="Адвокат по уголовным делам в Москве – помощь и консультации."
+        />
+        <meta name="yandex-verification" content="ca6674660fe1aaf4" />
+      </Head>
+
       {/* Верхний контейнер */}
       <section className={styles.heroSection}>
         <div className={styles.heroTextContainer}>
-          <h1 className={styles.heroTitle}>Адвокат по уголовным делам Песков В.С.</h1>
+          <h1 className={styles.heroTitle}>
+            Адвокат по уголовным делам Песков В.С.
+          </h1>
           <p className={styles.heroDescription}>
-            Опытный адвокат по уголовным делам, с успешной практикой на протяжении долгих лет.
+            Опытный адвокат по уголовным делам, с успешной практикой на
+            протяжении долгих лет.
           </p>
           <div className={styles.buttons}>
-            <a href="tel:+79165780936" className={styles.button}>Позвонить</a>
-            </div>
+            <a href="tel:+79165780936" className={styles.button}>
+              Позвонить
+            </a>
+          </div>
         </div>
       </section>
 
@@ -33,29 +40,29 @@ export default function HomePage(): JSX.Element {
 
       {/* Контейнер с 4 карточками */}
       <section className={styles.fourCardsSection}>
-  <div className={styles.cardWrapper}>
-    <div className={styles.serviceCard}>
-      <FaUserShield className={styles.cardIcon} />
-      <h2>Предварительное следствие</h2>
-      <p>Защита на стадии предварительного следствия по любой категории дел.</p>
-    </div>
-    <div className={styles.serviceCard}>
-      <FaBook className={styles.cardIcon} />
-      <h2>Допросы и дача показаний</h2>
-      <p>Защита на стадии допросов и дачи показаний по любой категории дел.</p>
-    </div>
-    <div className={styles.serviceCard}>
-      <FaGavel className={styles.cardIcon} />
-      <h2>Защита в суде</h2>
-      <p>Защита на стадии судебного разбирательства по любой категории дел.</p>
-    </div>
-    <div className={styles.serviceCard}>
-      <FaFileAlt className={styles.cardIcon} />
-      <h2>Составление жалоб</h2>
-      <p>Составление апелляционных, кассационных и иных жалоб необходимых для защиты.</p>
-    </div>
-  </div>
-</section>
+        <div className={styles.cardWrapper}>
+          <div className={styles.serviceCard}>
+            <FaUserShield className={styles.cardIcon} />
+            <h2>Предварительное следствие</h2>
+            <p>Защита на стадии предварительного следствия по любой категории дел.</p>
+          </div>
+          <div className={styles.serviceCard}>
+            <FaBook className={styles.cardIcon} />
+            <h2>Допросы и дача показаний</h2>
+            <p>Защита на стадии допросов и дачи показаний по любой категории дел.</p>
+          </div>
+          <div className={styles.serviceCard}>
+            <FaGavel className={styles.cardIcon} />
+            <h2>Защита в суде</h2>
+            <p>Защита на стадии судебного разбирательства по любой категории дел.</p>
+          </div>
+          <div className={styles.serviceCard}>
+            <FaFileAlt className={styles.cardIcon} />
+            <h2>Составление жалоб</h2>
+            <p>Составление апелляционных, кассационных и иных жалоб, необходимых для защиты.</p>
+          </div>
+        </div>
+      </section>
 
       {/* Контейнер с кнопкой "Все услуги" */}
       <section className={styles.allServicesButton}>
@@ -69,47 +76,42 @@ export default function HomePage(): JSX.Element {
 
       {/* Контейнер с 3 карточками (фото, текст) */}
       <section className={styles.threeCardsSection}>
-  <div className={styles.cardWrapper3card}>
-    <div className={styles.caseCard}>
-      <Link href="/cases">
-      <img
-        src="/next.svg"
-        alt="Адвокат по уголовным делам в Москве"
-        className={styles.caseImage}
-      />
-      </Link>
-      <h1>Дело о мошенничестве</h1>
-    </div>
-    <div className={styles.caseCard}>
-    <Link href="/cases">
-      <img
-        src="/vercel.svg"
-        alt="Юрист по уголовным делам в Москве"
-        className={styles.caseImage}
-      />      
-      </Link>
-      <p>Дело о краже</p>
-    </div>
-    <div className={styles.caseCard}>
-    <Link href="/cases">
-      <img
-        src="/window.svg"
-        alt="Адвокат по делам о наркотиках в Москве"
-        className={styles.caseImage}
-      />
-      </Link>
-      <p>Дело о наркотиках</p>
-    </div>
-  </div>
-</section>
+        <div className={styles.cardWrapper3card}>
+          <div className={styles.caseCard}>
+            <Link href="/cases">
+              <img
+                src="/next.svg"
+                alt="Адвокат по уголовным делам в Москве"
+                className={styles.caseImage}
+              />
+            </Link>
+            <h1>Дело о мошенничестве</h1>
+          </div>
+          <div className={styles.caseCard}>
+            <Link href="/cases">
+              <img
+                src="/vercel.svg"
+                alt="Юрист по уголовным делам в Москве"
+                className={styles.caseImage}
+              />
+            </Link>
+            <p>Дело о краже</p>
+          </div>
+          <div className={styles.caseCard}>
+            <Link href="/cases">
+              <img
+                src="/window.svg"
+                alt="Адвокат по делам о наркотиках в Москве"
+                className={styles.caseImage}
+              />
+            </Link>
+            <p>Дело о наркотиках</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
-
-
-
-
-
 
 
 
