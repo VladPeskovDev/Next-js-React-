f5910809013afa8cd8a0ee442522d1ede21aadd5
Владислав <vladislav@MacBook-Air-Vladislav.local>
2024-11-11 18:30:24 +0300

    Release3.3

--- lawyer-page/src/pages/api/yandexVerification.ts
+++ lawyer-page/src/pages/api/yandexVerification.ts
@@ -3,5 +3,5 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Content-Type', 'text/plain');
-  res.status(200).send('ca6674660fe1aaf4'); // строгое текстовое содержимое
+  res.status(200).send(' ca6674660fe1aaf4'); // строгое текстовое содержимое
 }
